b336b3b4c42e22a5af2ce5215053ba6213f0bb15
gracekfan <33182506+gracekfan@users.noreply.github.com>
2018-08-16 16:35:02 -0600

    update bg gradient colours (#23)
    background gradient changed to match design colours (six colours)
    
    percentages need to be updated when page grows in content

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -10,7 +10,7 @@ const movePage = keyframes`
 `;
 
 export const Background = styled.div`
-  background: linear-gradient(#91a9ca, #fdf4e9);
+  background: linear-gradient(0deg, #F9ECEE 0%, #F9DFE5 36.34%, #F9DEE4 38.4%, #E2DCEB 60.31%, #BAD9F7 99.44%, #FFFFFF 100%);  
   height: 100%;
   width: 100%;
   margin: 0;
