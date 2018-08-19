793650ed72b70bb6dc70346162f21f6e0fc6fe78
Jay Khatri <khatri.florida@gmail.com>
2018-08-19 00:36:57 -0700

    here's a first pass at a basic form template

--- src/HomePage/Content/SubmitField/SubmitField.js
+++ src/HomePage/Content/SubmitField/SubmitField.js
@@ -1,26 +1,58 @@
 import React from "react";
-import { Flex } from "grid-styled";
-import { Input, Button } from "antd";
-import { Margin } from "./SubmitFieldStyles.js";
+import { Form, Input, Button } from "antd";
 
-const SubmitField = props => (
-  <Margin>
-    <Flex flexDirection="row">
-      <Input
-        style={{
-          fontSize: props.fontSize,
-          width: props.isWebView ? "100%" : "200px",
-        }}
-        placeholder="Sign up for email updates!"
-      />
-      <Button
-        style={{ backgroundColor: "#FF7C93", borderColor: "#FF7C93" }}
-        type="primary"
-      >
-        Submit
-      </Button>
-    </Flex>
-  </Margin>
-);
+const FormItem = Form.Item;
 
-export default SubmitField;
+class SubmitField extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const { isWebView, fontSize } = this.props;
+
+    return (
+      <Form layout="inline" onSubmit={this.handleSubmit}>
+        <FormItem>
+          {getFieldDecorator("email", {
+            rules: [
+              {
+                type: "email",
+                message: "The input is not valid E-mail!"
+              },
+              {
+                required: true,
+                message: "Please input your E-mail!"
+              }
+            ]
+          })(
+            <Input
+              style={{
+                fontSize,
+                width: isWebView ? "300px" : "200px",
+              }}
+              placeholder="Sign up for email updates!"
+            />
+          )}
+        </FormItem>
+        <FormItem>
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{ backgroundColor: "#FF7C93", borderColor: "#FF7C93" }}
+          >
+            Submit
+          </Button>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+export default Form.create()(SubmitField);
