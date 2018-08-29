4b6cbe394963e94de618f3e2e660310b68d92759
Jay Khatri <khatri.florida@gmail.com>
2018-08-29 18:34:03 -0500

    adding new initial page

--- src/SponsorPage/SponsorPage.js
+++ src/SponsorPage/SponsorPage.js
@@ -1,26 +1,22 @@
 import React from "react";
 import { Flex } from "grid-styled";
 
-import { Background } from "./SponsorPageStyles.js";
-
 class SponsorPage extends React.Component {
   render () {
     const { isWebView } = this.props;
 
     return (
-      <Background>
-        <Flex
-          justifyContent={isWebView ? "initial" : "center"}
-          css={{
-            width: "100vw",
-            height: "100vh",
-            overflow: "hidden",
-            zIndex: "2"
-          }}
-        >
+      <Flex
+        justifyContent={isWebView ? "initial" : "center"}
+        css={{
+          width: "100vw",
+          height: "100vh",
+          overflow: "hidden",
+          zIndex: "2"
+        }}
+      >
           hello
-        </Flex>
-      </Background>
+      </Flex>
     );
   }
 }
