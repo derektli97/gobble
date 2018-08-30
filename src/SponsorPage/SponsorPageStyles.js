6f005229b136f0a3f15d2c3c79288231b739c6f4
Jay Khatri <khatri.florida@gmail.com>
2018-08-29 20:21:50 -0500

    addressing comment about color

--- src/SponsorPage/SponsorPageStyles.js
+++ src/SponsorPage/SponsorPageStyles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Background = styled.div`
-  background: linear-gradient(0deg, #F9ECEE 0%, #F9DFE5 36.34%, #F9DEE4 38.4%, #E2DCEB 60.31%, #BAD9F7 99.44%, #FFFFFF 100%);
+  background: linear-gradient(0deg, #F9ECEE 0%, #F9DFE5 36.34%, #F9DEE4 38.4%, #E2DCEB 60.31%, #BAD9F7 100%);
   height: 100%;
   width: 100%;
   margin: 0;
