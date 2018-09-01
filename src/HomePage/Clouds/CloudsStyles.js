6a2da32f408930bc94d8b286b10796fd67b1b3d7
Maria Theresia Tyas <theresia.valerie@gmail.com>
2018-09-01 16:06:45 -0500

    cloud animation

--- src/HomePage/Clouds/CloudsStyles.js
+++ src/HomePage/Clouds/CloudsStyles.js
@@ -1,10 +1,19 @@
 import styled, { keyframes } from "styled-components";
 
-const moveCloud = keyframes`
+const moveCloudRight = keyframes`
   0%{
-    transform: translate(-50vw, 0)
+    transform: translate(-10vw, 0)
   }
   100%{
+    transform: translate(100vw, 0)
+  }
+`;
+
+const moveCloudLeft = keyframes`
+  100%{
+    transform: translate(-10vw, 0)
+  }
+  0%{
     transform: translate(100vw, 0)
   }
 `;
@@ -15,12 +24,21 @@ export const StaticCloud = styled.img`
   zindex: 0;
 `;
 
-export const Cloud = styled.img`
+export const CloudMoveRight = styled.img`
   display: flex;
   position: absolute;
   zindex: 0;
   left: 0;
-  animation-name: ${moveCloud};
+  animation-name: ${moveCloudRight};
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+`;
+
+export const CloudMoveLeft = styled.img`
+  display: flex;
+  position: absolute;
+  zindex: 0;
+  animation-name: ${moveCloudLeft};
   animation-timing-function: linear;
   animation-iteration-count: infinite;
 `;
