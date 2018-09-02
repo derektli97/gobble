e6f7fde54d153fbd7766444249b975503fbec851
Jay Khatri <khatri.florida@gmail.com>
2018-09-01 20:27:40 -0500

    init

---
+++ src/EventsPage/EventsPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Flex } from "grid-styled";
+
+class SponsorPage extends React.Component {
+  render () {
+    const { isWebView } = this.props;
+
+    return (
+      <Flex
+        justifyContent={isWebView ? "initial" : "center"}
+        css={{
+          width: "100vw",
+          height: "100vh",
+          overflow: "hidden"
+        }}
+      >
+          test page
+      </Flex>
+    );
+  }
+}
+
+export default SponsorPage;
