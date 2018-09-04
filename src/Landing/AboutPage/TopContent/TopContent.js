54ca28ab5d4584ca94660b41d07914874fbf6cba
Muin Momin <momin.muin@gmail.com>
2018-09-04 10:01:32 -0500

    updated string on about section

--- src/Landing/AboutPage/TopContent/TopContent.js
+++ src/Landing/AboutPage/TopContent/TopContent.js
@@ -20,7 +20,7 @@ class TopContent extends React.Component {
           <AboutTitle {...{ isWebView }} >About</AboutTitle>
           <AboutSubtitle {...{ isWebView }} >"Hack" in hackathon is used to describe how technology can be used in a new and innovative way.</AboutSubtitle>
           <AboutParagraph {...{ isWebView }} >At TAMUhack, teams spend the weekend working on innovative software and hardware solutions to real-world problems in a community full of mentorship.</AboutParagraph>
-          <AboutParagraph {...{ isWebView }} >We'll provide you with all the resources and mentors you need to work on something cool. You'll have the freedom to create a product, learn the new skills or just have fun working on a project with friends. We'll  also provide all the free food and free swag&#8212;just be sure to bring plenty of resumes!</AboutParagraph>
+          <AboutParagraph {...{ isWebView }} >We'll provide you with all the resources and mentors you need to work on something cool. You'll have the freedom to create a product, learn new skills or just have fun working on a project with friends. We'll also provide all the free food and free swag&#8212;just be sure to bring plenty of resumes for all of the awesome sponsors and mentors you'll meet! Check them out below!</AboutParagraph>
         </div>
         <img
           alt=''
