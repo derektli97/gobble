4cad8f2e4493cab1f9c011687a5d7d28ee0346e6
Jay Khatri <khatri.florida@gmail.com>
2018-09-04 11:38:24 -0500

    changes to events.js

--- src/Events/Events.js
+++ src/Events/Events.js
@@ -1,21 +1,44 @@
 import React from "react";
-import { Flex } from "grid-styled";
+import EventsCard from "./EventsCard/EventsCard.js";
 
 class Events extends React.Component {
-  render () {
-    const { isWebView } = this.props;
+  // Tamuhack events are stored in a js object.
+  state = { events: null, isLoaded: false };
 
+  componentDidMount () {
+    // Hit our backend to get all events.
+    fetch("https://quack.tamuhack.com/events")
+      .then(response => response.json())
+      .then(events => this.setState({ events: events.events, isLoaded: true }));
+  }
+
+  // Parses an event, and return an EventsCard component.
+  parseEventCard (event) {
+    // TODO(all) Add more fields for the events and style the events card.
+    const { name } = event;
+    return (<EventsCard key={name.text} name={name.text} />);
+  }
+
+  // Checks for a valid event.
+  isValidEvent (event) {
+    const { name: { text } } = event;
+    if (text == null || text.length === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  render () {
+    const { isLoaded } = this.state;
     return (
-      <Flex
-        justifyContent={isWebView ? "initial" : "center"}
-        css={{
-          width: "100vw",
-          height: "100vh",
-          overflow: "hidden"
-        }}
-      >
-          test page
-      </Flex>
+      <div>
+        { isLoaded
+          ? this.state.events
+            .filter(this.isValidEvent)
+            .map(this.parseEventCard)
+          : <p> Loading ...</p>
+        }
+      </div>
     );
   }
 }
