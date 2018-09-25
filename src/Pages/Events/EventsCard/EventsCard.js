15c3feeb2cc3cbd6312007319a1021f7f4450ea2
Jay Khatri <khatri.florida@gmail.com>
2018-09-25 11:36:41 -0500

    i think its working, oh boy

--- src/Pages/Events/EventsCard/EventsCard.js
+++ src/Pages/Events/EventsCard/EventsCard.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Card, Tag, Button } from "antd";
+import { Flex } from "grid-styled";
+import { Card, Button } from "antd";
+import { EventCardTitle, EventCardDate } from "Pages/Events/EventsCard/EventsCardStyles";
 
 class EventsCard extends React.Component {
   render () {
     const { event } = this.props;
-    console.log(event);
+    const eventDateTime = new Date(event.start.local);
+    const localDateTime = new Date();
+    // If the event has already passed, return an empty element.
+    if (eventDateTime < localDateTime) {
+      return <React.Fragment />;
+    }
     return (
-      <Card bordered={false} style={{ width: 300, margin: "10px" }}>
-        <p>{ event.name.text }</p>
-        <Button>hello</Button>
-        <Tag>Tag 1</Tag>
+      <Card bordered={false} style={{ padding: "0px", width: 400, margin: "10px" }}>
+        <Flex justifyContent='space-between'>
+          <Flex flexDirection='column'>
+            <EventCardTitle>{ event.name.text }</EventCardTitle>
+            <EventCardDate>{ eventDateTime.toLocaleString("en-US", { hour12: true }) }</EventCardDate>
+          </Flex>
+          <Button target='_blank' href={event.url}>Sign Up</Button>
+        </Flex>
       </Card>
     );
   }
