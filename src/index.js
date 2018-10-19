40d541ba240da3f53c805ff4c0806e4965c8ee3e
Jay Khatri <khatri.florida@gmail.com>
2018-10-19 15:36:47 -0500

    init

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PageRouter from "Pages/PageRouter";
-import registerServiceWorker from "registerServiceWorker";
+import { unregister } from "registerServiceWorker";
 import "./index.css";
 
 ReactDOM.render(<PageRouter />, document.getElementById("root"));
-registerServiceWorker();
+unregister();
