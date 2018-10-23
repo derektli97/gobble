929aac8c67a236667b6cddcd63afb6e19262d1e4
Muin Momin <momin.muin@gmail.com>
2018-10-22 19:05:39 -0500

    Removed old sponsors section (#114)

--- src/Pages/Landing/SponsorPage/SponsorPage.js
+++ src/Pages/Landing/SponsorPage/SponsorPage.js
@@ -56,39 +56,6 @@ class SponsorPage extends React.Component {
               <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/facebook.png")} />
             </SponsorRow>
           </SponsorContainer>
-          <Flex style={{ justifyContent: "center", fontSize: "14px", flexDirection: "column" }}>
-            <SponsorTitleRow {...{ isWebView }} style={{ marginBottom: "0px" }}>
-              <SponsorTitle {...{ isWebView }}>
-              Previous Sponsors
-              </SponsorTitle>
-            </SponsorTitleRow>
-            <hr style={{ width: isWebView ? "350px" : "180px", border: "solid 2px #3F688D" }} />
-          </Flex>
-          <SponsorContainer>
-            <SponsorRow {...{ isWebView }}>
-              <SponsorLogoBig {...{ isWebView }} src={require("assets/SponsorLogos/AA.svg")} />
-            </SponsorRow>
-            <SponsorRow {...{ isWebView }}>
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/chase.png")} />
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/google.png")} />
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/microsoft.png")} />
-            </SponsorRow>
-            <SponsorRow {...{ isWebView }}>
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/southwest.png")} />
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/facebook.png")} />
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/capital_one.png")} />
-            </SponsorRow>
-            <SponsorRow {...{ isWebView }}>
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/accenture.png")} />
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/goldman.png")} />
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/esri.png")} />
-            </SponsorRow>
-            <SponsorRow {...{ isWebView }}>
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/hbk.png")} />
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/pariveda.png")} />
-              <SponsorLogo {...{ isWebView }} src={require("assets/SponsorLogos/gmnew.png")} />
-            </SponsorRow>
-          </SponsorContainer>
         </Flex>
       </Background>
     );
