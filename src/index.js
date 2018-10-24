2e8c2ad3e85b082093082a0075c99df85df607e4
Jay Khatri <khatri.florida@gmail.com>
2018-10-24 17:12:19 -0500

    change

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PageRouter from "Pages/PageRouter";
-import { unregister } from "registerServiceWorker";
 import "./index.css";
 
 ReactDOM.render(<PageRouter />, document.getElementById("root"));
-unregister();
