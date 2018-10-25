c5a092c817b8ae52db73d6ea6d09f5a9a5998cbf
Muin Momin <momin.muin@gmail.com>
2018-10-24 20:09:30 -0500

    "game night!!! be there or be square" - mkchoi212
    Huge fix! DO NOT REVERT

--- src/CommonComponents/Footer/Footer.js
+++ src/CommonComponents/Footer/Footer.js
@@ -45,7 +45,7 @@ class Footer extends React.Component {
           </a>
         </Flex>
         <Copyright {...{ isWebView }} >
-          Built with ❤ from TAMUhack
+          Made with ❤ by TAMUhack
         </Copyright>
       </Flex>
     );
