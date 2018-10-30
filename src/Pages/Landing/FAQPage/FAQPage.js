ec7e05fa1104acf2a59165640c69282884a934e0
amangotchu <aman.gottu@gmail.com>
2018-10-30 12:06:28 -0500

    Added hacky anchor tags

--- src/Pages/Landing/FAQPage/FAQPage.js
+++ src/Pages/Landing/FAQPage/FAQPage.js
@@ -32,17 +32,32 @@ class FAQPage extends React.Component {
             {this.faqDataKeys[this.state.activeQuestionIndex]}
           </QuestionTitle>
           <QuestionBody>
-            {faqData[this.faqDataKeys[this.state.activeQuestionIndex]].split("\n").map((item, key) => {
+            {console.log(faqData[this.faqDataKeys[this.state.activeQuestionIndex]].split("<").length)}
+            {faqData[this.faqDataKeys[this.state.activeQuestionIndex]].split("<").map((item, key) => {
+              var link = faqData[this.faqDataKeys[this.state.activeQuestionIndex]].split("<")[key];
               if (this.state.activeQuestionIndex === 0) {
                 return (
                   <p key={key}>{item} <br /> <br />
                     <p style={{ borderLeft: "12px solid #ddd", paddingLeft: "25px", maxWidth: "480px", fontSize: "16px" }}>
-                      <p style={{ fontWeight: 600, margin: 0, fontSize: "19px" }}>NOTE</p> We are able to admit minors only if they are accompanied by a college student (18+) who is planning on participating in the hackathon. Have additional questions? Email us at highschool@tamuhack.com
+                      <p style={{ fontWeight: 600, margin: 0, fontSize: "19px", href: "highschool@tamuhack.com" }}>NOTE</p> We are able to admit minors only if they are accompanied by a college student (18+) who is planning on participating in the hackathon.
+                      Have additional questions? Email us at <a href='mailto:highschool@tamuhack.com'>highschool@tamuhack.com</a>
                     </p>
                   </p>
                 );
+              } else if (this.state.activeQuestionIndex !== 11) {
+                if (key !== 0 && key !== faqData[this.faqDataKeys[this.state.activeQuestionIndex]].split("<").length - 1) {
+                  return (
+                    <a href={link} target='_blank'>{item}</a>
+                  );
+                } else {
+                  return <p key={key}>{item}</p>;
+                }
               } else {
-                return <p key={key}>{item} <br /></p>;
+                if (key === 0) {
+                  return (
+                    <p>Email us with any questions, comments, concerns at <a href='mailto:hello@tamuhack.com'>hello@tamuhack.com</a> or message us here!</p>
+                  );
+                }
               }
             })}
           </QuestionBody>
