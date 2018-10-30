9ded2f01d3cbdf6ba3c062a42892cb015091c3be
amangotchu <aman.gottu@gmail.com>
2018-10-30 14:58:23 -0500

    Added Linkify to FAQ

--- src/Pages/Landing/FAQPage/FAQPage.js
+++ src/Pages/Landing/FAQPage/FAQPage.js
@@ -37,9 +37,9 @@ class FAQPage extends React.Component {
               if (this.state.activeQuestionIndex === 0) {
                 return (
                   <p key={key}>{item} <br /> <br />
-                    <p style={{ borderLeft: "12px solid #ddd", paddingLeft: "25px", maxWidth: "480px", fontSize: "16px" }}>
+                    <Linkify style={{ borderLeft: "12px solid #ddd", paddingLeft: "25px", maxWidth: "480px", fontSize: "16px" }}>
                       <p style={{ fontWeight: 600, margin: 0, fontSize: "19px" }}>NOTE</p> We are able to admit minors only if they are accompanied by a college student (18+) who is planning on participating in the hackathon. Have additional questions? Email us at highschool@tamuhack.com
-                    </p>
+                    </Linkify>
                   </p>
                 );
               } else {
