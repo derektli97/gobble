3ee52bf84727d8c643f3b81cee4b74ff168ece86
Aman Gottumukkala <aman.gottu@gmail.com>
2018-10-30 14:27:22 -0700

    Added hacky anchor tags (#128)
    * Added hacky anchor tags
    
    * I think i fixed it
    
    * Added Linkify to FAQ

--- src/Pages/Landing/FAQPage/FAQPage.js
+++ src/Pages/Landing/FAQPage/FAQPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Linkify from "react-linkify";
 
 import {
   Background,
@@ -36,13 +37,13 @@ class FAQPage extends React.Component {
               if (this.state.activeQuestionIndex === 0) {
                 return (
                   <p key={key}>{item} <br /> <br />
-                    <p style={{ borderLeft: "12px solid #ddd", paddingLeft: "25px", maxWidth: "480px", fontSize: "16px" }}>
+                    <Linkify style={{ borderLeft: "12px solid #ddd", paddingLeft: "25px", maxWidth: "480px", fontSize: "16px" }}>
                       <p style={{ fontWeight: 600, margin: 0, fontSize: "19px" }}>NOTE</p> We are able to admit minors only if they are accompanied by a college student (18+) who is planning on participating in the hackathon. Have additional questions? Email us at highschool@tamuhack.com
-                    </p>
+                    </Linkify>
                   </p>
                 );
               } else {
-                return <p key={key}>{item} <br /></p>;
+                return <Linkify properties={{ target: "_blank", rel: "nofollow   noopener" }} key={key}>{item} <br /></Linkify>;
               }
             })}
           </QuestionBody>
