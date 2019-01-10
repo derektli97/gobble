56e73ed2b6deb835a21dcba6cfa5e664c4a53544
amangotchu <aman.gottu@gmail.com>
2019-01-10 00:21:27 -0600

    Added base navbar for live.

--- src/Pages/Live/Live.js
+++ src/Pages/Live/Live.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Background } from "Pages/Events/EventsStyles";
+import LiveNavBar from "Pages/Live/LiveNavbar";
 
 class Live extends React.Component {
+  state = { hello: "hello" };
+
+  constructor (props) {
+    super(props);
+    this.hello = "hello";
+  }
+
   render () {
+    const { isWebView } = this.props;
     return (
-      <Background>
-          hello
+      <Background style={{ height: "100vh" }}>
+        <LiveNavBar {...{ isWebView }} />
       </Background>
     );
   }
